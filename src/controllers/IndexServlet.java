dd5f1b3741269ee60e7eead8bd4e5a0ac624dd1b
seish <seish@my_laptop>
2022-11-09 19:02:55 +0900

    課題修正中

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -39,8 +39,9 @@ public class IndexServlet extends HttpServlet {
         em.close();
 
         request.setAttribute("tasks", tasks);
-        }
 
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/index.jsp");
-        rd.forward(request,  response);
+    RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/index.jsp");
+    rd.forward(request, response);
     }
+}
+
